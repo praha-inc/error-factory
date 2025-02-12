281c98148a7ba2116fc29d7b5519dfb8ad81e02b
Karibash <y.karibayashi@gmail.com>
2025-02-12 23:35:54 +0900

    feature: Add a function to create a custom error

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,31 @@
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
-import { greeting } from './index';
+import { ErrorFactory } from './index';
 
-describe('greeting', () => {
-  it('logs "Hello, World!" to the console', () => {
-    const consoleSpy = vi.spyOn(console, 'log');
+describe('ErrorFactory', () => {
+  it('should set the correct name on the created error', () => {
+    class TestError extends ErrorFactory({ name: 'TestError', message: 'Test error' }) {}
 
-    greeting();
+    const error = new TestError();
 
-    expect(consoleSpy).toHaveBeenCalledWith('Hello, World!');
+    expect(error.name).toBe('TestError');
+  });
+
+  it('should set the correct message on the created error', () => {
+    class TestError extends ErrorFactory({ name: 'TestError', message: 'Test error' }) {}
+
+    const error = new TestError();
+
+    expect(error.message).toBe('Test error');
+  });
+
+  it('should allow passing the cause option to the created error', () => {
+    class TestError extends ErrorFactory({ name: 'TestError', message: 'Test error' }) {}
+
+    const cause = new Error('Cause error');
+    const error = new TestError({ cause });
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-member-access
+    expect((error as any)['cause']).toBe(cause);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-export const greeting = () => {
-  console.log('Hello, World!');
+export const ErrorFactory = <Name extends string>(
+  { name, message }: { name: Name; message: string },
+): new(options?: ErrorOptions) => Error & Readonly<{ name: Name }> => {
+  abstract class Class extends Error {
+    public override readonly name = name;
+    public static readonly message = message;
+
+    protected constructor(options?: ErrorOptions) {
+      super(Class.message, options);
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-return
+  return Class as any;
 };
